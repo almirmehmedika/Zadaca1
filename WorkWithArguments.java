ebd2ea5f56e5ef962ad1995976ed9d0ab5c6e51a
Becirr <becir.omerbasic@bitcamp.ba>
2015-06-08 21:45:21 +0200

    Zadaca

---
+++ WorkWithArguments.java
@@ -0,0 +1,44 @@
+public class WorkWithArguments {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		double[] array = new double[args.length];
+		if (args.length == 0) {
+			System.out.println("Moras unijeti makar jedan parametar");
+			System.exit(0);
+		}
+		for (int i = 0; i < args.length; i++) {
+
+			 
+				try {
+					array[i] = Double.parseDouble(args[i]);
+					
+				} catch (NumberFormatException ex) {
+					System.out.println("Unio si slovo ili znak.");
+					System.out.println("Sljedeci put unesi broj");
+					System.exit(0);
+				}
+				
+			if (array[i] == 0) {
+				System.out.println("Ne smijes dati nulu kao parametar. ");
+				System.out.println("Sad moras ponovo pokrenuti program");
+				System.exit(0);
+			}
+		}
+		
+		for (int i = 0; i < args.length; i++) {
+			System.out.printf("%.2f", array[i]);
+			if (i == args.length - 1) {
+				break;
+			}
+			System.out.print(", ");
+		}
+		System.out.println();
+		double product = 1;
+		for (int i = 0; i < args.length; i++) {
+			product *= array[i];
+		}
+		System.out.printf("Product is: %f", product);
+	}
+
+}
---
+++ ZadacaIzParametara.java
@@ -0,0 +1,22 @@
+
+public class ZadacaIzParametara {
+
+	public static void main(String[] args) {
+		System.out.println(squareNumber(3));
+		System.out.println(squareDoubleNumber(3.2));
+		System.out.println(productTwoNumber(3 , 4));
+		System.out.println(productTwoDoubleNumber(3.1, 3.3));
+	}
+	public static int squareNumber(int a){
+		return a*a;
+	}
+	public static double squareDoubleNumber(double a){
+		return a*a;
+	}
+	public static int productTwoNumber(int a, int b){
+		return a*b;
+	}
+	public static double productTwoDoubleNumber(double a, double b){
+		return a*b;
+	}
+}
