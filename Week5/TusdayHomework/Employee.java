c00e709e222775b32d75d20c526ef711118ee1e6
almirmehmedika <almir.mehmedika@bicamp.ba>
2015-06-17 17:51:14 +0200

    Update Employee.java

--- Week5/TusdayHomework/Employee.java
+++ Week5/TusdayHomework/Employee.java
@@ -1,32 +1,86 @@
+/**
+ * @author
+ *
+ */
+public class Employee
+{
+    private String name;
+    private String gender;
+    private int salary;
 
-public class Employee {
-	private String name;
-	private String gender;
-	private int salary;
-	
-	//Generating constructor
-	public Employee(String n, String g, int s) {
-		name = n;
-		gender = g;
-		salary = s;
-	}
-	//Generating getters
-	
-	public String getName() {
-		return name;
-	}
+    /**
+     * 
+     */
+    public Employee()
+    {
+        this(null, null, 0); // Mozes cak reci da npr, svaki novi ima gender = M
+                             // tj, this(null, "M", 0); i ako se radi o zeni
+                             // onda samo proslijediti Gender = W
+    }
 
-	public String getGender() {
-		return gender;
-	}
+    // Generating constructor
+    /**
+     * @param name
+     * @param gender
+     * @param salary
+     */
+    private Employee(final String name, final String gender, final int salary)
+    {
+        this.name = name;
+        this.gender = gender;
+        this.salary = salary;
+    }
 
-	public int getSalary() {
-		return salary;
-	}
-	// Generating setters
-	public void setSalary(int salary) {
-		this.salary = salary;
-	}
-	
-	
+    // Generating getters
+
+    /**
+     * @return name
+     */
+    public String getName()
+    {
+        return name;
+    }
+
+    /**
+     * @param name
+     *            the name to set
+     */
+    public void setName(final String name)
+    {
+        this.name = name;
+    }
+
+    /**
+     * @param gender
+     *            the gender to set
+     */
+    public void setGender(final String gender)
+    {
+        this.gender = gender;
+    }
+
+    /**
+     * @return gender
+     */
+    public String getGender()
+    {
+        return gender;
+    }
+
+    /**
+     * @return salary
+     */
+    public int getSalary()
+    {
+        return salary;
+    }
+
+    // Generating setters
+    /**
+     * @param salary
+     */
+    public void setSalary(final int salary)
+    {
+        this.salary = salary;
+    }
 }
